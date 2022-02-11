e27ff309147a39c481ba279e7025cff94057b811
Lucas Guedes <lucas.guedes1778@gmail.com>
2022-02-11 14:08:15 -0300

    postgres

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
+import email
 from django.db import models
 
-# Create your models here.
+
+class GrupoCliente(models.Model):
+    descricao = models.CharField(u'Descrição', max_length=64)
+    nomecontato = models.CharField(u'Nome do Contato', max_length=64)
+    telefone = models.CharField(u'Telefon', max_length=25)
+    email = models.CharField(u'Email', max_length=128)
+    apiuser = models.CharField(u'Username API', max_length=256)
+    apipass = models.CharField(u'Password API', max_length=256)
+    apitoken = models.CharField(u'Token API', max_length=256)
+    dtcadastro = models.DateTimeField(u'Data do Cadastro', auto_now=True)
+    status = models.IntegerField(u'Ativo')
+
+    def __str__(self) :
+        return self.descricao
